17e94965171aa6c84b60ef178fd71af90449614e
Heitor Garcia de Souza <heitorfrutal96@gmail.com>
2021-03-23 17:02:45 -0300

    Initial commit

---
+++ app/src/main/java/android/example/tarefa/MainActivity.kt
@@ -0,0 +1,14 @@
+package android.example.tarefa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "DEBUG");
+        Log.e("MainActivity", "ERROR");
+    }
+}
